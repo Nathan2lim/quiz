10a7a8ec4da5b5d56bc1d892e912fa286d5869d0
Nonoka <noestm77@gmail.com>
2025-02-04 12:23:55 +0100

    Add background image and enhance layout for Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,28 +39,30 @@ const Home = () => {
 
     return (
         <div className='home'>
-        <h1>Quiz Game</h1>
-        <h2>Liste des quiz</h2>
-        {loading && <p>Chargement...</p>}
-        {error && <p className="text-red-500">{error}</p>}
+            <div className='home__container'>
+                <h1>Quiz Game</h1>
+                <h2>Liste des quiz</h2>
+                {loading && <p>Chargement...</p>}
+                {error && <p className="text-red-500">{error}</p>}
 
-        <div className="w-full max-w-3xl bg-white shadow-md rounded-lg p-4">
-            {quizzes.length === 0 && !loading && <p>Aucun quiz disponible.</p>}
-            {quizzes.map((quiz) => (
-            <div key={quiz._id} className="border-b py-4">
-                <h2 className="text-lg font-semibold">{quiz.title}</h2>
-                <p className="text-gray-500 text-sm">{quiz.description}</p>
-                <button
-                onClick={() => handleStartQuiz(quiz._id)}
-                className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                >
-                Lancer le Quiz
-                </button>
+                <div className="w-full max-w-3xl bg-white shadow-md rounded-lg p-4">
+                    {quizzes.length === 0 && !loading && <p>Aucun quiz disponible.</p>}
+                    {quizzes.map((quiz) => (
+                    <div key={quiz._id} className="border-b py-4">
+                        <h2 className="text-lg font-semibold">{quiz.title}</h2>
+                        <p className="text-gray-500 text-sm">{quiz.description}</p>
+                        <button
+                        onClick={() => handleStartQuiz(quiz._id)}
+                        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                        >
+                        Lancer le Quiz
+                        </button>
+                    </div>
+                    ))}
+                </div>
+
+                <ButtonComponent name="Jouer" onClick={() => navigate('game')}/>
             </div>
-            ))}
-        </div>
-
-        <ButtonComponent name="Jouer" onClick={() => navigate('game')}/>
         </div>
     );
 };
